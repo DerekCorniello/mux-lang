f44c112e4ff124ed951d66a2c98a6b839f16e7c3
Derek Corniello <corniedj@mail.uc.edu>
2025-09-08 14:24:59 -0400

    formatted code

--- mux-compiler/src/parser.rs
+++ mux-compiler/src/parser.rs
@@ -10,55 +10,70 @@ pub enum AstNode {
 
 #[derive(Debug, Clone)]
 pub enum DeclarationNode {
-    Function { 
-        name: String, 
+    Function {
+        name: String,
         type_params: Vec<(String, Vec<TraitBound>)>,
-        params: Vec<Param>, 
-        return_type: TypeNode, 
-        body: Vec<StatementNode> 
+        params: Vec<Param>,
+        return_type: TypeNode,
+        body: Vec<StatementNode>,
     },
-    Class { 
+    Class {
         name: String,
         type_params: Vec<(String, Vec<TraitBound>)>,
         traits: Vec<TraitRef>,
-        fields: Vec<Field>, 
-        methods: Vec<FunctionNode> 
+        fields: Vec<Field>,
+        methods: Vec<FunctionNode>,
     },
-    Interface { 
+    Interface {
         name: String,
         type_params: Vec<(String, Vec<TraitBound>)>,
-        methods: Vec<FunctionNode> 
+        methods: Vec<FunctionNode>,
     },
-    Enum { 
+    Enum {
         name: String,
         type_params: Vec<(String, Vec<TraitBound>)>,
-        variants: Vec<EnumVariant> 
+        variants: Vec<EnumVariant>,
     },
     TypeAlias {
         name: String,
         type_params: Vec<(String, Vec<TraitBound>)>,
         target: TypeNode,
     },
-    Const { 
-        name: String, 
-        type_: Option<TypeNode>, 
-        value: ExpressionNode 
+    Const {
+        name: String,
+        type_: Option<TypeNode>,
+        value: ExpressionNode,
     },
-    Let { 
-        name: String, 
-        type_: Option<TypeNode>, 
-        value: ExpressionNode 
+    Let {
+        name: String,
+        type_: Option<TypeNode>,
+        value: ExpressionNode,
     },
 }
 
 #[derive(Debug, Clone)]
 pub enum StatementNode {
-    Let { name: String, type_: Option<TypeNode>, value: ExpressionNode },
+    Let {
+        name: String,
+        type_: Option<TypeNode>,
+        value: ExpressionNode,
+    },
     Return(Option<ExpressionNode>),
     Expression(ExpressionNode),
-    If { cond: ExpressionNode, then_block: Vec<StatementNode>, else_block: Option<Vec<StatementNode>> },
-    While { cond: ExpressionNode, body: Vec<StatementNode> },
-    For { var: String, iter: ExpressionNode, body: Vec<StatementNode> },
+    If {
+        cond: ExpressionNode,
+        then_block: Vec<StatementNode>,
+        else_block: Option<Vec<StatementNode>>,
+    },
+    While {
+        cond: ExpressionNode,
+        body: Vec<StatementNode>,
+    },
+    For {
+        var: String,
+        iter: ExpressionNode,
+        body: Vec<StatementNode>,
+    },
     Break,
     Continue,
     Block(Vec<StatementNode>),
@@ -68,14 +83,40 @@ pub enum StatementNode {
 pub enum ExpressionNode {
     Literal(LiteralNode),
     Identifier(String),
-    Binary { left: Box<ExpressionNode>, op: BinaryOp, right: Box<ExpressionNode> },
-    Unary { op: UnaryOp, expr: Box<ExpressionNode> },
-    Call { func: Box<ExpressionNode>, args: Vec<ExpressionNode> },
-    FieldAccess { expr: Box<ExpressionNode>, field: String },
-    ArrayAccess { expr: Box<ExpressionNode>, index: Box<ExpressionNode> },
-    Lambda { params: Vec<Param>, body: Vec<StatementNode> },
-    IfExpr { cond: Box<ExpressionNode>, then_block: Vec<StatementNode>, else_block: Option<Vec<StatementNode>> },
-    Match { expr: Box<ExpressionNode>, arms: Vec<MatchArm> },
+    Binary {
+        left: Box<ExpressionNode>,
+        op: BinaryOp,
+        right: Box<ExpressionNode>,
+    },
+    Unary {
+        op: UnaryOp,
+        expr: Box<ExpressionNode>,
+    },
+    Call {
+        func: Box<ExpressionNode>,
+        args: Vec<ExpressionNode>,
+    },
+    FieldAccess {
+        expr: Box<ExpressionNode>,
+        field: String,
+    },
+    ArrayAccess {
+        expr: Box<ExpressionNode>,
+        index: Box<ExpressionNode>,
+    },
+    Lambda {
+        params: Vec<Param>,
+        body: Vec<StatementNode>,
+    },
+    IfExpr {
+        cond: Box<ExpressionNode>,
+        then_block: Vec<StatementNode>,
+        else_block: Option<Vec<StatementNode>>,
+    },
+    Match {
+        expr: Box<ExpressionNode>,
+        arms: Vec<MatchArm>,
+    },
 }
 
 #[derive(Debug, Clone)]
@@ -91,8 +132,8 @@ pub enum PrimitiveType {
 pub enum TypeNode {
     Primitive(PrimitiveType),
     Named(String),
-    Generic { 
-        base: String, 
+    Generic {
+        base: String,
         args: Vec<TypeNode>,
         bounds: Vec<TraitBound>,
     },
@@ -168,22 +209,37 @@ pub enum PatternNode {
     Identifier(String),
     Wildcard, // `_`
     Tuple(Vec<PatternNode>),
-    EnumVariant { name: String, args: Vec<PatternNode> },
+    EnumVariant {
+        name: String,
+        args: Vec<PatternNode>,
+    },
 }
 
 #[derive(Debug, Clone)]
 pub enum BinaryOp {
-    Add, Sub, Mul, Div, Mod,
-    Eq, Neq, Lt, Gt, Leq, Geq,
-    And, Or,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    Neq,
+    Lt,
+    Gt,
+    Leq,
+    Geq,
+    And,
+    Or,
 }
 
 #[derive(Debug, Clone)]
 pub enum UnaryOp {
-    Neg, Not,
+    Neg,
+    Not,
 }
 
+fn parse_from_tokens(tokens: Vec<Token>) -> Vec<AstNode> {
+    let parsed_result: Vec<AstNode> = Vec::new();
 
-fn parse_from_tokens(tokens: Vec<Token>) -> AstNode {
-
+    parsed_result
 }
