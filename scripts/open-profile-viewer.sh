582fed8a7f0657f0c653e985a2e0e4f22363e764
Derek Corniello <corniedj@mail.uc.edu>
2026-04-28 13:26:23 -0400

    impl

--- scripts/open-profile-viewer.sh
+++ scripts/open-profile-viewer.sh
@@ -9,4 +9,4 @@ if [[ ${1:-} == "-h" || ${1:-} == "--help" ]]; then
   exit 0
 fi
 
-"$repo_root/scripts/dev-cargo.sh" run --bin mux-profile-viewer -p mux-profile-viewer -- "$@"
+"$repo_root/scripts/dev-cargo.sh" run -p mux-profiling --features mux-profiling/viewer --bin mux-profile-viewer -- "$@"
